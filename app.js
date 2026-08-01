00eb40ea3bfa66a58afee3c1b8ce6846605459f4
Raghukongala <kongalaraghu1999@gmail.com>
2026-08-01 23:02:54 +0530

    Add application health endpoint

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
 console.log("==================================");
 console.log("ABC BANKING APPLICATION");
 console.log("Environment : Development");
@@ -5,3 +11,22 @@ console.log("Version : 1.0.1");
 console.log("Login Feature Added");
 console.log("GitHub Enterprise Lab");
 console.log("==================================");
+
+
+app.get("/", (req, res) => {
+    res.send("ABC Banking Application Running");
+});
+
+
+app.get("/health", (req, res) => {
+    res.status(200).json({
+        status: "UP",
+        application: "banking-api",
+        version: "1.0.1"
+    });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
