78e4fb85b429da9091aaffd2b44352fae8b8aa3f
Raghukongala <kongalaraghu1999@gmail.com>
2026-08-01 17:24:22 +0530

    Added login feature

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 console.log("==================================");
 console.log("ABC BANKING APPLICATION");
 console.log("Environment : Development");
-console.log("Version : 1.0.0");
+console.log("Version : 1.0.1");
+console.log("Login Feature Added");
 console.log("GitHub Enterprise Lab");
 console.log("==================================");
