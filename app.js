509624a92d6efe2715f6929ea65557b1d1797b27
Raghukongala <kongalaraghu1999@gmail.com>
2026-08-01 17:16:47 +0530

    Initial CI pipeline with Docker support

---
+++ app.js
@@ -0,0 +1,6 @@
+console.log("==================================");
+console.log("ABC BANKING APPLICATION");
+console.log("Environment : Development");
+console.log("Version : 1.0.0");
+console.log("GitHub Enterprise Lab");
+console.log("==================================");
